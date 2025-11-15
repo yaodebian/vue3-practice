1ee2d523f72afbe13c22662b7958a64d3f6e0341
yaodebian <2801540120@qq.com>
2025-11-15 16:55:43 +0800

    feat: add store examples

--- vue3-vite/src/views/AboutView.vue
+++ vue3-vite/src/views/AboutView.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+// 在组件内部的任何地方均可以访问变量 `store` ✨
+const store = useCounterStore()
+
+/* print initial value */
+console.log(store.count)
+console.log(store.doubleCount)
+
+/* increment value after 3 seconds */
+setTimeout(() => {
+  store.increment()
+  /* print updated value */
+  console.log(store.count)
+  console.log(store.doubleCount)
+}, 3000)
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
--- vue3-vite/src/views/HomeView.vue
+++ vue3-vite/src/views/HomeView.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+
+import { useCounterStore } from '@/stores/counter'
+// 在组件内部的任何地方均可以访问变量 `store` ✨
+const store = useCounterStore()
+
+/* print initial value */
+console.log(store.count)
+console.log(store.doubleCount)
+
+/* increment value after 3 seconds */
+setTimeout(() => {
+  store.increment()
+  /* print updated value */
+  console.log(store.count)
+  console.log(store.doubleCount)
+}, 3000)
 </script>
 
 <template>
